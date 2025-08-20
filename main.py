71de038c35103c74e23e7e00e02e35bcd6c4be9f
dariagorsk <daria.gorskaia@code.berlin>
2025-08-20 12:07:05 +0200

    Added else condition, so that the game substracts an attempt in case of a wrong letter, and also displays the amount of attempts remaining. Added: the win condition (if guessed letters within 5 attempts form a secret word), the losing condition (no attempts left)

--- main.py
+++ main.py
@@ -6,22 +6,32 @@ attempt = 0
 max_attempts = 5
 
 word = random.choice(secret_words)
-hidden_word = "_ ".join("_" for ch in word)
+hidden_word = ["_" for ch in word]
 
 while attempt < max_attempts:
-    guess = input(f"Welcome to the game \"Hangman\"! Your goal is to discover a secret word or phrase by guessing letters of the alphabet. Enter only one letter at a time. You have only 5 wrong guesses. The secret word: {hidden_word}. Let's start: ").lower()
+    print(f"Welcome to the game \"Hangman\"! Your goal is to discover a secret word or phrase by guessing letters of the alphabet. Enter only one letter at a time. You have only 5 wrong guesses.") 
+    guess = input(f"The secret word: {" ".join(hidden_word)}. Let's start: ").lower()
 
-    if not guess.isalpha() or len(guess) != 1:
-    
     if not guess.isalpha() or len(guess) != 1:
         print("Please, enter only 1 letter! Let's try again: ")
-    continue
+        continue
 
     if guess in guessed_letters:
-    print("You already guessed that letter!")
-    continue
+        print("You already guessed that letter!")
+        continue
+
     guessed_letters.append(guess)
 
-if not guess in word:
-    print("Nope! The letter is not in the secret word. Try again: ")
-    continue
+    if guess in word:
+        hidden_word = [guess if ch == guess else hidden_word[index] for index, ch in enumerate(word)]
+        print("Correct!", " ".join(hidden_word))
+    else:
+        attempt += 1
+        print(f"Nope! The letter is not in the word. Attempts remaining: {max_attempts - attempt}")
+
+    if guessed_letters == word:
+        print("Great! You won!")
+        break
+
+if attempt >= max_attempts:
+    print("Sorry, you lost :(")
