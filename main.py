1dafd45a2bb727b6b13bbf3a715122bc971dbeee
dariagorsk <daria.gorskaia@code.berlin>
2025-08-20 11:52:16 +0200

    added while loop, so that the game would get going only within 5 attempts (but not calculating attempts yet in the code), put the join function inside the condition, because there was an error when using enumerate, but then the hidden word was displayed incorrectly in the greeting message, so I added join function in the input directly as well

--- main.py
+++ main.py
@@ -8,16 +8,19 @@ max_attempts = 5
 word = random.choice(secret_words)
 hidden_word = "_ ".join("_" for ch in word)
 
-guess = input(f"Welcome to the game \"Hangman\"! Your goal is to discover a secret word or phrase by guessing letters of the alphabet. Enter only one letter at a time. You have only 5 wrong guesses. The secret word: {hidden_word}. Let's start: ").lower()
+while attempt < max_attempts:
+    guess = input(f"Welcome to the game \"Hangman\"! Your goal is to discover a secret word or phrase by guessing letters of the alphabet. Enter only one letter at a time. You have only 5 wrong guesses. The secret word: {hidden_word}. Let's start: ").lower()
 
-if not guess.isalpha() or len(guess) != 1:
-    print("Please, enter only 1 letter! Let's try again: ")
+    if not guess.isalpha() or len(guess) != 1:
+    
+    if not guess.isalpha() or len(guess) != 1:
+        print("Please, enter only 1 letter! Let's try again: ")
     continue
 
-if guess in guessed_letters:
+    if guess in guessed_letters:
     print("You already guessed that letter!")
     continue
-guessed_letters.append(guess)
+    guessed_letters.append(guess)
 
 if not guess in word:
     print("Nope! The letter is not in the secret word. Try again: ")
