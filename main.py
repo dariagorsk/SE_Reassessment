c547f634a61c58d138d158a7f31c295270240229
dariagorsk <daria.gorskaia@code.berlin>
2025-08-20 10:55:32 +0200

    Added few functions: checking in an input is a letter or is not equal to 1 symbol, then returns to the beginning of the loop; if an input is already in the storage, then goes to the beginning of the loop, otherwise the letter is stored on the list (guessed_letters); if an input is not in the secet word, then returns to the beginning of the loop

--- main.py
+++ main.py
@@ -7,4 +7,18 @@ max_attempts = 5
 
 word = random.choice(secret_words)
 hidden_word = "_ ".join("_" for ch in word)
-print(f"Welcome to the game \"Hangman\"! Your goal is to discover a secret word or phrase by guessing letters of the alphabet. Enter only one letter at a time. You have only 5 wrong guesses. The secret word: {hidden_word} ")
+
+guess = input(f"Welcome to the game \"Hangman\"! Your goal is to discover a secret word or phrase by guessing letters of the alphabet. Enter only one letter at a time. You have only 5 wrong guesses. The secret word: {hidden_word}. Let's start: ").lower()
+
+if not guess.isalpha() or len(guess) != 1:
+    print("Please, enter only 1 letter! Let's try again: ")
+    continue
+
+if guess in guessed_letters:
+    print("You already guessed that letter!")
+    continue
+guessed_letters.append(guess)
+
+if not guess in word:
+    print("Nope! The letter is not in the secret word. Try again: ")
+    continue
